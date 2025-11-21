a5e9df7fe5f879d64289e2f031918a1d8e5e5315
goodluck-aberare <aberaregoodluck@yahoo.com>
2025-11-21 23:55:25 +0100

    v1

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using Models;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("123 Main St", "Dallas", "TX", "USA");
+        Customer customer1 = new Customer("Goodluck Aberare", address1);
+
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Keyboard", "K100", 25.99, 2));
+        order1.AddProduct(new Product("Mouse", "M200", 15.50, 1));
+
+        Console.WriteLine(order1.PackingLabel());
+        Console.WriteLine(order1.ShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.TotalCost()}");
     }
 }
