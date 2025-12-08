534fb619dbb52ed1129386105ac331070d1bb140
goodluck-aberare <aberaregoodluck@yahoo.com>
2025-12-08 15:39:07 +0100

    v1

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,26 @@
+using System;
+
+abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime Date => _date;
+    public int Minutes => _minutes;
+
+    public abstract double GetDistance(); // in kilometers
+    public abstract double GetSpeed();    // in km/h
+    public abstract double GetPace();     // in min/km
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {this.GetType().Name} ({_minutes} min): " +
+               $"Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
+}
